8285d30d800e0deedc26c03d5671e2c15ecb56a8
Danijel Trifunović <danijel.trifunovic@factory.hr>
2020-09-12 18:01:54 +0200

    added spinner

--- MobileNetworkInventoryManager/Utilities/Extensions/UIView+Extension.swift
+++ MobileNetworkInventoryManager/Utilities/Extensions/UIView+Extension.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 public extension UIView {
-    func addSubviews(_ views: UIView...){
+    func addSubviews(_ views: UIView...) {
         for view in views{
             self.addSubview(view)
         }
--- MobileNetworkInventoryManager/Utilities/Extensions/UIViewController+Extension.swift
+++ MobileNetworkInventoryManager/Utilities/Extensions/UIViewController+Extension.swift
@@ -14,7 +14,34 @@ public extension UIViewController {
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+}
+
+public extension UIViewController {
+    static var spinner: UIView?
+    
+    func showSpinner(on view: UIView) {
+        let spinnerView = UIView.init(frame: view.bounds)
+        spinnerView.backgroundColor = UIColor.darkGray.withAlphaComponent(0.5)
+        let activityIndicator = UIActivityIndicatorView.init(style: .whiteLarge)
+        activityIndicator.startAnimating()
+        activityIndicator.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(activityIndicator)
+            view.addSubview(spinnerView)
+        }
+        
+        UIViewController.spinner = spinnerView
+    }
+    
+    func removeSpinner() {
+        DispatchQueue.main.async {
+            UIViewController.spinner?.removeFromSuperview()
+            UIViewController.spinner = nil
+        }
     }
 }
